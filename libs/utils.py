509f25076e6ff24c3edf8aa146e085d7d514f436
vongkhmer <vutheavong.kh@gmail.com>
2021-05-20 14:22:17 +0900

    Add .gitignore to empty directory

--- libs/utils.py
+++ libs/utils.py
@@ -42,12 +42,14 @@ def process_img():
         print("Downloading coco dataset")
         download_coco()
 
-    original_image_id, original_keypoints, original_bbox = read_annotation_json()
+    original_image_id, original_keypoints, original_bbox, num_keypoints = read_annotation_json()
   
     Config.num_keypoints = len(original_keypoints[0]) // 3
 
     print(original_image_id[:10])
     print(Config.num_keypoints)
+
+    print("Start processing images according to configuration..")
 
     for i in range(len(original_image_id)):
         if i > 1000:
@@ -56,6 +58,8 @@ def process_img():
         img = Image.open(original_image_id[ind])
         w, h = img.size
         keyp = original_keypoints[ind]
+        num_key = num_keypoints[ind]
+
         bx, by, bw, bh = original_bbox[ind]
         bsz, cx, cy = max(bw, bh) * Config.bbx_multiplier, bx + bw//2, by + bh//2
 
@@ -101,21 +105,23 @@ def process_img():
 def download_coco():
     print("Downloading annotation file...")
     wget.download(coco_annotation_url, data_dir)
+
+    print("Extracting annotations...")
     with ZipFile(os.path.join(data_dir, "annotations_trainval2017.zip")) as zf:
         zf.extractall(data_dir)
-    print("Done.")
+    print("\nDone.")
 
     print("Downloading training data.")
-
     wget.download(coco_dataset_url, data_dir)
+
     print("Extracting training data...")
     with ZipFile(os.path.join(data_dir, "train2017.zip")) as zf:
         zf.extractall(data_dir)
-
-    print("Done.")
+    print("\nDone.")
 
 
 def read_annotation_json():
+    print("Reading annotation json..")
     with open(os.path.join(annotaion_dir, "person_keypoints_train2017.json"), "r") as f:
         annotations = json.load(f)
 
@@ -123,6 +129,8 @@ def read_annotation_json():
     original_image_id = []
     original_keypoints = []
     original_bbox = []
+    num_keypoints = []
+
 
     for a in annotations:
         image_id, keyp, bbox, num = a["image_id"], a["keypoints"], a["bbox"], a["num_keypoints"]
@@ -134,8 +142,12 @@ def read_annotation_json():
         original_image_id.append(image_id)
         original_keypoints.append(keyp)
         original_bbox.append(bbox)
+        num_keypoints.append(num)
+    
+    print("Done.")
 
-    return original_image_id, original_keypoints, original_bbox
+
+    return original_image_id, original_keypoints, original_bbox, num_keypoints
 
 
 def reset():
